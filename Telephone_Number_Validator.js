f218e16e1ade2868ca188ebfe2af65e688223cfd
Natalia Doskach <natalia.a.doskach@gmail.com>
2018-10-31 14:58:28 +0800

    wrote description

---
+++ Telephone_Number_Validator.js
@@ -0,0 +1,10 @@
+
+/*Returns true if the passed string looks like a valid US phone number.
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+For example:
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555*/
