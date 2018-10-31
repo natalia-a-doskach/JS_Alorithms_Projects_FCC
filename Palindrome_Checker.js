c5ffea213f28f0f2c50e3c6aac5b7064f038d5c6
Natalia Doskach <natalia.a.doskach@gmail.com>
2018-10-31 14:48:23 +0800

    wrote program description

---
+++ Palindrome_Checker.js
@@ -0,0 +1,16 @@
+/*Returns true if the given string is a palindrome. Otherwise, return false.
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+Strings with varying formats, such as "racecar", "RaceCar", and "race CAR" can be checked.
+Strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2" can be checked.*/
+
+
+
+
+function palindrome(str) {
+  // Good luck!
+  return true;
+}
+
+
+
+palindrome("eye");
